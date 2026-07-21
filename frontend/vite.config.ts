3669d9222ae29143c7c7948c535b1fd07c1dfcbd
lazYaaaa <lazariev.iaroslav@mail.ru>
2026-07-22 00:16:57 +0300

    parametrs && nginx added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['aquariums.chickenkiller.com'],
     proxy: {
       '/api': 'http://localhost:8080',
       '/healthz': 'http://localhost:8080',
