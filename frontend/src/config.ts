ee6a7d3ba0d54f9ad3b5d741dd9b85cad2ddd564
lazYaaaa <lazariev.iaroslav@mail.ru>
2026-07-23 04:26:05 +0300

    auth

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const SKIP_AUTH = import.meta.env.VITE_SKIP_AUTH === 'false';
+export const SKIP_AUTH = import.meta.env.VITE_SKIP_AUTH === 'true';
 
 export const DEV_USER = {
   username: 'dev',
